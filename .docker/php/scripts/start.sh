581ef46202a4a23bc1cc15ddb1f5f47ad85b96b1
dariopad <dario.pad@gmail.com>
2020-10-17 18:18:11 +0200

    First release

---
+++ .docker/php/scripts/start.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+cd /var/www/
+
+# Setup git variables
+if [ ! -z "$GIT_EMAIL" ]; then
+    git config --global user.email "$GIT_EMAIL"
+fi
+if [ ! -z "$GIT_NAME" ]; then
+    git config --global user.name "$GIT_NAME"
+    git config --global push.default simple
+fi
+if [[ "$GIT_USE_SSH" == "1" ]] ; then
+    # Disable Strict Host checking for non interactive git clones
+    mkdir -p -m 0700 /root/.ssh
+    # Prevent config files from being filled to infinity by force of stop and restart the container 
+    echo "" > /root/.ssh/config
+    echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
+    echo -e "Host *\n\tUser ${GIT_USERNAME}\n\n" >> /root/.ssh/config
+    if [ ! -z "$SSH_KEY" ]; then
+        echo $SSH_KEY > /root/.ssh/id_rsa.base64
+        base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
+        chmod 600 /root/.ssh/id_rsa
+    fi
+fi
+
+if [ ! -f /var/www/.env ]; then
+    # Pull down code from git for our site!
+    if [ ! -z "$CLONE_REPO" ] && [ ! -z "$GIT_REPO" ]; then
+        # Remove the test index file if you are pulling in a git repo
+        if [ ! -z ${REMOVE_FILES} ] && [ ${REMOVE_FILES} == 0 ]; then
+            echo "skiping removal of files"
+        else
+            rm -Rf /var/www/*
+        fi
+        GIT_COMMAND='git clone '
+        if [ ! -z "$GIT_BRANCH" ]; then
+            GIT_COMMAND=${GIT_COMMAND}" -b ${GIT_BRANCH}"
+        fi
+
+        if [ -z "$GIT_USERNAME" ] && [ -z "$GIT_PERSONAL_TOKEN" ]; then
+            GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
+        else
+            if [[ "$GIT_USE_SSH" == "1" ]]; then
+            GIT_COMMAND=${GIT_COMMAND}" ${GIT_REPO}"
+            else
+            GIT_COMMAND=${GIT_COMMAND}" https://${GIT_USERNAME}:${GIT_PERSONAL_TOKEN}@${GIT_REPO}"
+            fi
+        fi
+        echo "Cloning now....."
+        #${GIT_COMMAND} /var/www || exit 1
+        #ssh-agent bash -c 'ssh-add /somewhere/yourkey; git clone git@github.com:user/project.git'
+        ${GIT_COMMAND} . || exit 1
+        if [ ! -z "$GIT_TAG" ]; then
+            git checkout ${GIT_TAG} || exit 1
+        fi
+        if [ ! -z "$GIT_COMMIT" ]; then
+            git checkout ${GIT_COMMIT} || exit 1
+        fi
+        if [ -z "$SKIP_CHOWN" ]; then
+            chown -Rf $user.www-data /var/www
+        fi
+
+    fi
+
+    echo "Copying Laravel environment file... "
+    if [ -f /var/temp/.env ]; then
+            cp /var/temp/.env .
+            rm -fR /var/temp/*
+    fi
+
+    if [ -f /var/www/composer.json ]; then     
+        if [ -z "$SKIP_COMPOSER_UPDATE" ]; then        
+            echo "Running composer update... "
+            composer global require hirak/prestissimo
+            COMPOSER_MEMORY_LIMIT=4G COMPOSER_ALLOW_XDEBUG=1 composer update
+        fi
+        echo "Dump autoload and generate key... "
+        composer dump-autoload && php artisan key:generate
+    fi
+
+fi
+
---
+++ Dockerfile.php
@@ -0,0 +1,83 @@
+#Step 1
+FROM php:7.2-fpm-alpine3.8
+
+#Step 2
+ARG uid
+
+#Step 3
+ARG user
+
+#Step 4
+WORKDIR /var/www
+
+#Step 5
+RUN echo "*** Upgrading Alpine! ***" \
+    && apk update && apk upgrade \
+    && echo "*** Adding php libraries! ***" \
+    && apk add --no-cache curl libbz2 php7-bz2 php7-pdo php7-pgsql php7-bcmath php7-zmq php7-curl bash php7-pear php7-imagick openssh nano supervisor\
+    libtool postgresql-dev libpng-dev imagemagick-c++ imagemagick-dev libmcrypt-dev libxml2-dev \
+    yaml-dev bzip2 aspell-dev autoconf build-base linux-headers libaio-dev zlib-dev git subversion freetype-dev \
+    libjpeg-turbo-dev libmcrypt-dev bzip2-dev libstdc++ libxslt-dev openldap-dev hiredis-dev make unzip \
+    ffmpeg wget 
+    #&& apk cache clean
+
+#Step 6
+RUN echo "*** Installing Composer! ***" \
+    && curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer \
+    && echo "*** Installing PHP extentions! ***" \
+    && docker-php-ext-install gd bcmath zip bz2 pdo pdo_pgsql simplexml opcache sockets mbstring pcntl xsl pspell opcache pdo_mysql \
+    && echo "*** Configuring PHP GD extension! ***" \
+    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
+    && echo "*** Installing Imagick extension! ***" \
+    && pecl install imagick \
+    && echo "*** Enabling PHP Imagick extentions! ***" \
+    && docker-php-ext-enable imagick \
+    && echo "*** Installing PHP Redis extentions! ***" \
+    && pecl install -o -f redis \
+    && echo "*** Enabling Redis extention! ***" \
+    && docker-php-ext-enable redis \
+    && echo "*** Installing PHP XDebug extention! ***" \
+    && pecl install xdebug \
+    && wget http://xdebug.org/files/xdebug-2.6.1.tgz \
+    && tar -xvzf xdebug-2.6.1.tgz \
+    && cd xdebug-2.6.1 \
+    && echo "*** Configuring XDebug! ***" \
+    && phpize \
+    && ./configure --enable-xdebug \
+    && make \
+    && make install \
+    && cp modules/xdebug.so /usr/local/lib/php/extensions/no-debug-non-zts-20170718 \
+    && echo 'zend_extension = /usr/local/lib/php/extensions/no-debug-non-zts-20170718/xdebug.so' >> /usr/local/etc/php/php.ini \
+    && echo 'zend_extension = /usr/local/lib/php/extensions/no-debug-non-zts-20170718/xdebug.so' >> /etc/php7/php.ini \
+    && echo 'xdebug.remote_enable=true' >> /etc/php7/php.ini \
+    && echo 'xdebug.remote_host=127.0.0.1' >> /etc/php7/php.ini \
+    && echo 'xdebug.remote_port=9000' >> /etc/php7/php.ini \
+    && echo 'xdebug.remote_handler=dbgp' >> /etc/php7/php.ini \
+    && echo 'xdebug.max_nesting_level=512' >> /etc/php7/php.ini \
+    && rm -rf /tmp/pear
+
+#Step 7
+# Add user for laravel application
+RUN echo "*** Adding user! ***" \
+    && adduser -S -D --ingroup www-data --uid $uid -h /home/$user --shell /bin/bash $user \
+    && chown -R $user:www-data .
+
+#Step 8
+ADD .docker/php/scripts/start.sh /start.sh
+
+#Step 9
+RUN mkdir -p /var/temp \
+    && chown -R $user:www-data /var/temp \
+    && chown $user:www-data /start.sh \
+    && chmod 775 /start.sh
+
+#Step 10
+ADD .docker/php/laravel/.env /var/temp
+
+#Step 11
+# Change current user to www
+USER $user
+
+#Step 13
+# RUN configuration script
+#CMD ["/start.sh", "$user"]
